0342e1cac5dde4e46ec8a9646ed066e90142c27f
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2017-09-11 19:57:42 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("ENTER ANY INTEGER");
+scanf("%d",&n);
+if(n==0)
+{
+printf("GIVEN NUMBER IS EVEN");
+else
+printf("GIVEN NUMBER IS ODD");
+}
+return 0;
+}
