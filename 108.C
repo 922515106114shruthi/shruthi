7e26ffe2322317a7503a200bd4dd747b15b17f53
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-03-19 16:37:45 +0530

    Create 108.C

---
+++ 108.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+            int A,B,C,d,n;
+            scanf("%d",&n);
+            printf("enter the values :");
+            scanf("%d,%d,%d",&A,&B,&C);
+            d=(n/2*(A+C));
+            printf("%d",d);
+            getch();
+ }                 
