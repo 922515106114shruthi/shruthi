f94a14b20b4016d8dabd1c599fc1bf3a2fd6aaa0
922515106114shruthi <31506030+922515106114shruthi@users.noreply.github.com>
2018-03-19 15:55:35 +0530

    Create 78.C

---
+++ 78.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n;
+  printf("enter the number:");
+  scanf("%d",&n);
+  if((n%13)==0)
+  {
+  	printf("yes, %d is multi[ple of 13",n);
+  }else
+  {
+  	printf("no, %d is not multi[ple of 13",n);
+    }
+    getch();
+    }
